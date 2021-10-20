45df7d8616bd55dcbcad36b8b3eeab6945fe9f67
raximvodil <raximvodilrozbek@gmail.com>
2021-10-20 15:10:40 +0500

    generete started , must start create link front !

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,10 +1,35 @@
 const { Router } = require("express");
+const config = require("config");
+const shortid = require("shortid");
 const Link = require("../models/Link");
 const auth = require("../middleware/auth.middleware");
 const router = Router();
 
-router.post("/generate", async (req, res) => {
+router.post("/generate", auth, async (req, res) => {
   try {
+    const baseUrl = config.get("baseUrl");
+    const { from } = req.body;
+
+    const code = shortid.generate();
+
+    const existing = await Link.findOne({ from });
+
+    if (existing) {
+      return res.json({ link: existing });
+    }
+
+    const to = baseUrl + "/t/" + code;
+
+    const link = new Link({
+      code,
+      to,
+      from,
+      owner: req.user.userId,
+    });
+
+    await link.save();
+
+    res.status(201).json({ link });
   } catch (e) {
     res.status(500).json({ message: "Some-where is wrong, try again !" });
   }
@@ -19,7 +44,7 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   try {
     const link = await Link.findById(req.params.id);
     res.json(link);
